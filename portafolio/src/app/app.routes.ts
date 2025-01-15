bbd85e132098cd69993201b0230fcc9d5c8a1010
ikerCH-03 <ikercordon2003.ich@gmail.com>
2025-01-15 17:47:30 +0100

    Comenzando un nuevo portfolio.

--- portafolio/src/app/app.routes.ts
+++ portafolio/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { MostrarPortComponent } from './pages/mostrar-port/mostrar-port.component';
+/*import { MostrarPortComponent } from './pages/mostrar-port/mostrar-port.component';*/
 import { HomeComponent } from './pages/home/home.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'portafolio', component: MostrarPortComponent },
+    { path: '', component: WelcomeComponent },
+    { path: 'home', component:  HomeComponent},
     { path: '**',redirectTo:'',pathMatch:'full'}
 ];
