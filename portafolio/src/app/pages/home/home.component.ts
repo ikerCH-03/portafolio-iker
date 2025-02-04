3159246d0e1ea88c9ed09fb5cd9bf05908fa1aa5
ikerCH-03 <ikercordon2003.ich@gmail.com>
2025-02-04 20:04:49 +0100

    Comenzando el home component.

--- portafolio/src/app/pages/home/home.component.ts
+++ portafolio/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
